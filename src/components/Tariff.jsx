4197ff4d43cefbd2f22aa2705b6700c6dffcd682
JuliaMichaela <juliaperez@yandex.ru>
2023-09-30 23:07:42 +0300

    changed Tariff

--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -1,7 +1,26 @@
+import React, { useState } from "react";
+import "../styles/tariff.css";
 
+export default function Tariff(props) {
 
-export default function Tariff() {
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleChange = () => {
+        setIsSelected(!isSelected);
+    }
+
+    const classCard = (isSelected ? "selected" : "");
+
     return (
-        <div>Tariff</div>
-    )
+        <div onClick={handleChange} className={`card ${classCard}`}>
+            <header className={`card_header ${props.theme.card_header}`}>Безлимитный  {props.rate}</header>
+            <div className={`card_price ${props.theme.card_price}`}>
+                <p className="card_value">руб </p>
+                <p className="card_number">{props.rate}</p>
+                <p className="card_date"> /мес</p>
+            </div>
+            <div className="card_speed">До {props.speed} Мбит/сек </div>
+            <footer className="card_footer">Объём включенного трафика не ограничен</footer>
+        </div>
+    );
 }
