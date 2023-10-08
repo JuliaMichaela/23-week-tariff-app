7d3cc7064700fb36ffb882491737275613d97d78
JuliaMichaela <juliaperez@yandex.ru>
2023-10-08 23:02:32 +0300

    added useState

--- src/components/Tariff.jsx
+++ src/components/Tariff.jsx
@@ -1,11 +1,19 @@
+import React, { useState } from "react";
 import "../styles/tariff.css";
 
 export default function Tariff(props) {
 
-    const classCard = (props.isSelected ? "selected" : "");
+    const [isSelected, setIsSelected] = useState(false);
+
+    const handleChange = () => {
+        setIsSelected(!isSelected);
+    }
+
+
+    const classCard = (isSelected ? "selected" : "");
 
     return (
-        <div className={`card ${classCard}`}>
+        <div className={`card ${classCard}`} onClick={handleChange}>
             <header className={`card_header ${props.theme.card_header}`}>Безлимитный  {props.rate}</header>
             <div className={`card_price ${props.theme.card_price}`}>
                 <p className="card_value">руб </p>
